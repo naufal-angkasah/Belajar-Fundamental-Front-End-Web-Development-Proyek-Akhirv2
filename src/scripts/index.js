5ae46afd39ffa7274bd39b1fccfd178a37dc7882
Naufal Angkasah <ahmad.naufalangkasah@gmail.com>
2024-06-10 19:52:41 +0700

    v.1

---
+++ src/scripts/index.js
@@ -0,0 +1,25 @@
+import 'regenerator-runtime';
+import '../styles/main.css';
+import '../styles/responsive.css';
+import App from './views/app';
+import swRegister from './utils/sw-register';
+import 'lazysizes';
+import 'lazysizes/plugins/parent-fit/ls.parent-fit';
+
+const START = 10;
+const NUMBER_OF_IMAGES = 100;
+
+const app = new App({
+  button: document.querySelector('#icon-menu'),
+  nav: document.querySelector('#navbar'),
+  content: document.querySelector('#thecontent'),
+});
+
+window.addEventListener('hashchange', () => {
+  app.renderPage();
+});
+
+window.addEventListener('load', () => {
+  app.renderPage();
+  swRegister();
+});
